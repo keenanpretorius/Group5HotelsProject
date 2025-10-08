0c07c98c39e13ee3f58e3901bc732a6bf9852813
Tim Sasman <timsasman@outlook.com>
2025-10-08 11:30:49 +0200

    add reception and person

---
+++ Group5HotelsProject/Business/Person.cs
@@ -0,0 +1,15 @@
+﻿
+
+    public class Person
+    {
+    public string id;
+    public string name;
+    public string address;
+    public string phone;
+    public string email;
+    public Person()
+    {
+    }
+    
+}
+
---
+++ Group5HotelsProject/Business/Receptionist.cs
@@ -0,0 +1,11 @@
+﻿public class Receptionist
+{
+    private String ReceptionistID;
+    private String Username;
+    private String hotelID;
+    private double wage;
+    private int Hour;
+    public Receptionist()
+    {
+    }
+}
