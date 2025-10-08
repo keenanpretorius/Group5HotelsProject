ab37a14cf0c2dcfdb285e1840707f23f79a4d851
Tim Sasman <timsasman@outlook.com>
2025-10-08 16:03:13 +0200

    change cancel booking form

---
+++ Group5HotelsProject/Presentation/ChangeCancelBookingForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Group5HotelsProject.Presentation
+{
+    partial class ChangeCancelBookingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            TermLabel = new Label();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(TermLabel);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(66, 45);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(807, 296);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Search by:";
+            // 
+            // TermLabel
+            // 
+            TermLabel.AutoSize = true;
+            TermLabel.Location = new Point(230, 57);
+            TermLabel.Name = "TermLabel";
+            TermLabel.Size = new Size(0, 15);
+            TermLabel.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(386, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(23, 86);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(949, 738);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label TermLabel;
+        private Label label1;
+    }
+}
