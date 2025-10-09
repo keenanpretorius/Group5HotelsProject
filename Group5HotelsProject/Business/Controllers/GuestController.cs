408bc8283a90fa602c5f13ada6236820225ad7a3
Keenan Pretorius <keenanpretorius1@gmail.com>
2025-10-09 17:19:35 +0200

    Completed Guest Controller and updated GuestDB

---
+++ Group5HotelsProject/Business/Controllers/GuestController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Group5HotelsProject.Business;
+using Group5HotelsProject.Data;
+
+namespace Group5HotelsProject.Controllers
+{
+    public class GuestController
+    {
+        #region Attributes
+        private GuestDB guestDB;
+        private Collection<Guest> guests;
+        #endregion
+
+        #region Properties
+        public Collection<Guest> AllGuests
+        {
+            get { return guests; }
+        }
+        #endregion
+
+        #region Constructors
+        public GuestController()
+        {
+            guestDB = new GuestDB();
+            guests = guestDB.AllGuests;
+        }
+        #endregion
+
+        #region CRUD Methods
+        public void AddGuest(Guest guest)
+        {
+            if (guest == null)
+                throw new ArgumentNullException(nameof(guest), "Guest cannot be null.");
+
+            guestDB.DataSetChange(guest, DB.DBOperation.Add);
+            guestDB.UpdateDataSource(guest);
+        }
+
+        public void EditGuest(Guest updatedGuest)
+        {
+            if (updatedGuest == null)
+                throw new ArgumentNullException(nameof(updatedGuest), "Updated guest cannot be null.");
+
+            guestDB.DataSetChange(updatedGuest, DB.DBOperation.Edit);
+            guestDB.UpdateDataSource(updatedGuest);
+        }
+
+        public void DeleteGuest(Guest guest)
+        {
+            if (guest == null)
+                throw new ArgumentNullException(nameof(guest), "Guest to delete cannot be null.");
+
+            guestDB.DataSetChange(guest, DB.DBOperation.Delete);
+            guestDB.UpdateDataSource(guest);
+        }
+        #endregion
+
+        #region Lookup / Query Methods
+        public Guest FindGuest(int guestID)
+        {
+            foreach (Guest g in guests)
+            {
+                if (g.GuestID == guestID)
+                    return g;
+            }
+            return null;
+        }
+
+        public Collection<Guest> GetGuestsByLoyaltyStatus(string loyaltyStatus)
+        {
+            Collection<Guest> results = new Collection<Guest>();
+            foreach (Guest g in guests)
+            {
+                if (string.Equals(g.LoyaltyStatus, loyaltyStatus, StringComparison.OrdinalIgnoreCase))
+                {
+                    results.Add(g);
+                }
+            }
+            return results;
+        }
+        #endregion
+
+        #region Utility Methods
+        public void RefreshFromDatabase()
+        {
+            guestDB = new GuestDB();
+            guests = guestDB.AllGuests;
+        }
+        #endregion
+    }
+}
--- Group5HotelsProject/Data/GuestDB.cs
+++ Group5HotelsProject/Data/GuestDB.cs
@@ -1,52 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using Group5HotelsProject.Business;
 
 namespace Group5HotelsProject.Data
 {
     public class GuestDB : DB
     {
+        #region Attributes
         private string table = "Guest";
         private string sql = "SELECT * FROM Guest";
         private Collection<Guest> guests;
+        #endregion
 
-        public Collection<Guest> AllGuests => guests;
+        #region Properties
+        public Collection<Guest> AllGuests
+        {
+            get { return guests; }
+        }
+        #endregion
 
+        #region Constructors
         public GuestDB()
         {
             guests = new Collection<Guest>();
             FillDataSet(sql, table);
             Add2Collection();
         }
+        #endregion
 
+        #region Private Methods
         private void Add2Collection()
         {
             foreach (DataRow row in dsMain.Tables[table].Rows)
             {
                 if (row.RowState != DataRowState.Deleted)
                 {
-                    Guest guest = new Guest
-                    {
-                        GuestID = Convert.ToInt32(row["GuestID"]),
-                        FirstName = row["FirstName"].ToString(),
-                        LastName = row["LastName"].ToString(),
-                        Address = row["_Address"].ToString(),
-                        City = row["City"].ToString(),
-                        PostalCode = row["PostalCode"].ToString(),
-                        PhoneNumber = (row["Phonenumber"]).ToString(),
-                        Email = row["Email"].ToString(),
-                        IDPassportNumber = (row["idpassportnumber"]).ToString()
-                    }
+                    Guest guest = new Guest();
+                    guest.GuestID = Convert.ToInt32(row["GuestID"]);
+                    guest.FirstName = row["FirstName"].ToString();
+                    guest.LastName = row["LastName"].ToString();
+                    guest.Address = row["_Address"].ToString();
+                    guest.City = row["City"].ToString();
+                    guest.PostalCode = row["PostalCode"].ToString();
+                    guest.PhoneNumber = row["Phonenumber"].ToString();
+                    guest.Email = row["Email"].ToString();
+                    guest.IDPassportNumber = row["idpassportnumber"].ToString();
+                    guest.LoyaltyStatus = row["LoyaltyStatus"].ToString();
+
                     guests.Add(guest);
                 }
             }
         }
+        #endregion
+
+        #region Public Methods
+        public void DataSetChange(Guest guest, DBOperation operation)
+        {
+            DataRow row = null;
+
+            switch (operation)
+            {
+                case DBOperation.Add:
+                    row = dsMain.Tables[table].NewRow();
+                    row["GuestID"] = guest.GuestID;
+                    row["FirstName"] = guest.FirstName;
+                    row["LastName"] = guest.LastName;
+                    row["_Address"] = guest.Address;
+                    row["City"] = guest.City;
+                    row["PostalCode"] = guest.PostalCode;
+                    row["Phonenumber"] = guest.PhoneNumber;
+                    row["Email"] = guest.Email;
+                    row["idpassportnumber"] = guest.IDPassportNumber;
+                    row["LoyaltyStatus"] = guest.LoyaltyStatus;
+                    dsMain.Tables[table].Rows.Add(row);
+                    guests.Add(guest);
+                    break;
+
+                case DBOperation.Edit:
+                    foreach (DataRow r in dsMain.Tables[table].Rows)
+                    {
+                        if (Convert.ToInt32(r["GuestID"]) == guest.GuestID)
+                        {
+                            r["FirstName"] = guest.FirstName;
+                            r["LastName"] = guest.LastName;
+                            r["_Address"] = guest.Address;
+                            r["City"] = guest.City;
+                            r["PostalCode"] = guest.PostalCode;
+                            r["Phonenumber"] = guest.PhoneNumber;
+                            r["Email"] = guest.Email;
+                            r["idpassportnumber"] = guest.IDPassportNumber;
+                            r["LoyaltyStatus"] = guest.LoyaltyStatus;
+                            break;
+                        }
+                    }
+                    break;
+
+                case DBOperation.Delete:
+                    for (int i = 0; i < dsMain.Tables[table].Rows.Count; i++)
+                    {
+                        if (Convert.ToInt32(dsMain.Tables[table].Rows[i]["GuestID"]) == guest.GuestID)
+                        {
+                            dsMain.Tables[table].Rows[i].Delete();
+                            break;
+                        }
+                    }
+                    guests.Remove(guest);
+                    break;
+            }
+        }
+
+        public void UpdateDataSource(Guest guest)
+        {
+            UpdateDataSource(sql, table);
+        }
+        #endregion
     }
 }
